1d8e1c78f741b2f61d7b3f5cdc00072d7ef0d8c0
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-05-27 19:52:19 +0300

    refactor(palette): added sin calculation to color intensity, and refactored the math to it's own functions

--- src/palette.c
+++ src/palette.c
@@ -12,149 +12,103 @@
 
 #include <fractal.h>
 
-static void	init_classic_palette(t_fractal_data *data)
+static int	flame_color(int iteration)
 {
-	int	i;
 	int	intensity;
-	int	blue;
-	int	green;
 	int	red;
+	int	green;
+	int	blue;
 
-	i = 0;
-	while (i <= MAX_ITERATIONS)
+	intensity = (iteration * 255) / MAX_ITERATIONS;
+	if (intensity < 85)
 	{
-		if (i == MAX_ITERATIONS)
-		{
-			data->color_map[i] = 0x00000000;
-		}
-		else
-		{
-			intensity = (i * 255) / MAX_ITERATIONS;
-			blue = 128 + (intensity / 2);
-			green = intensity / 3;
-			if (intensity > 128)
-				red = (intensity - 128) * 2;
-			else
-				red = 0;
-			data->color_map[i] = (red << 16) | (green << 8) | blue;
-		}
-		i++;
+		red = intensity * 2;
+		green = 0;
+		blue = intensity / 3;
 	}
+	else if (intensity < 170)
+	{
+		red = 255;
+		green = (intensity - 85) * 2;
+		blue = 0;
+	}
+	else
+	{
+		red = 255;
+		green = 255;
+		blue = (intensity - 170) * 3;
+	}
+	return ((red << 16) | (green << 8) | blue);
 }
 
-static void	init_flame_palette(t_fractal_data *data)
+static int	classic_color(int iteration)
 {
-	int	i;
 	int	intensity;
-	int	red;
-	int	green;
 	int	blue;
+	int	green;
+	int	red;
 
-	i = 0;
-	while (i <= MAX_ITERATIONS)
-	{
-		if (i == MAX_ITERATIONS)
-			data->color_map[i] = 0x000000;
-		else
-		{
-			intensity = (i * 255) / MAX_ITERATIONS;
-			if (intensity < 85)
-			{
-				red = intensity * 2;
-				green = 0;
-				blue = intensity / 3;
-			}
-			else if (intensity < 170)
-			{
-				red = 255;
-				green = (intensity - 85) * 2;
-				blue = 0;
-			}
-			else
-			{
-				red = 255;
-				green = 255;
-				blue = (intensity - 170) * 3;
-			}
-			data->color_map[i] = (red << 16) | (green << 8) | blue;
-		}
-		i++;
-	}
+	intensity = get_sin_intensity(iteration);
+	blue = 128 + (intensity / 2);
+	green = intensity / 3;
+	if (intensity > 128)
+		red = (intensity - 128) * 2;
+	else
+		red = 0;
+	return ((red << 16) | (green << 8) | blue);
 }
 
-static void	init_pastel_palette(t_fractal_data *data)
+static int	pastel_color(int iteration)
 {
-	int	i;
 	int	intensity;
 	int	red;
-	int	blue;
 	int	green;
+	int	blue;
 
-	i = 0;
-	while (i <= MAX_ITERATIONS)
-	{
-		if (i == MAX_ITERATIONS)
-			data->color_map[i] = 0x2C2C2C;
-		else
-		{
-			intensity = (i * 255) / MAX_ITERATIONS;
-			red = 180 + (intensity / 4);
-			green = 150 + (intensity / 6);
-			blue = 200 + (intensity / 5);
-			if (red > 255)
-				red = 255;
-			if (green > 255)
-				green = 255;
-			if (blue > 255)
-				blue = 255;
-			data->color_map[i] = (red << 16) | (green << 8) | blue;
-		}
-		i++;
-	}
+	intensity = get_sin_intensity(iteration);
+	red = 180 + (intensity / 2);
+	green = 150 + (intensity / 3);
+	blue = 200 + (intensity / 2);
+	if (red > 255)
+		red = 255;
+	if (green > 255)
+		green = 255;
+	if (blue > 255)
+		blue = 255;
+	return ((red << 16) | (green << 8) | blue);
 }
 
-static void	init_noir_palette(t_fractal_data *data)
+static int	noir_color(int iteration)
 {
-	int	i;
 	int	intensity;
 	int	red;
 	int	green;
 	int	blue;
 
-	i = 0;
-	while (i <= MAX_ITERATIONS)
+	intensity = get_sin_intensity(iteration);
+	if (intensity < 200)
 	{
-		if (i == MAX_ITERATIONS)
-			data->color_map[i] = 0x000000;
-		else
-		{
-			intensity = (i * 255) / MAX_ITERATIONS;
-			if (intensity < 200)
-			{
-				red = intensity;
-				green = intensity;
-				blue = intensity;
-			}
-			else
-			{
-				red = 255;
-				green = intensity / 4;
-				blue = intensity / 4;
-			}
-			data->color_map[i] = (red << 16) | (green << 8) | blue;
-		}
-		i++;
+		red = intensity;
+		green = intensity;
+		blue = intensity;
 	}
+	else
+	{
+		red = 255;
+		green = intensity / 4;
+		blue = intensity / 4;
+	}
+	return ((red << 16) | (green << 8) | blue);
 }
 
 void	init_palette(t_fractal_data *data)
 {
 	if (data->current_palette == 0)
-		init_classic_palette(data);
+		fill_color_map(data, classic_color, 0x00000000);
 	if (data->current_palette == 1)
-		init_flame_palette(data);
+		fill_color_map(data, flame_color, 0x000000);
 	if (data->current_palette == 2)
-		init_pastel_palette(data);
+		fill_color_map(data, pastel_color, 0x2C2C2C);
 	if (data->current_palette == 3)
-		init_noir_palette(data);
+		fill_color_map(data, noir_color, 0x000000);
 }

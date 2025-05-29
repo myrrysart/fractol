822ed56fe8d6e9cf6961f31f4641a935ade753c0
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-05-29 17:40:09 +0300

    depracation: Removed an unused function

--- src/palette_utils.c
+++ src/palette_utils.c
@@ -6,21 +6,11 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 18:09:18 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/05/27 18:09:23 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/05/29 11:51:22 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fractal.h>
-
-int	get_sin_intensity(int iteration)
-{
-	double	normalized;
-	double	sin_value;
-
-	normalized = (double)iteration / MAX_ITERATIONS;
-	sin_value = sin(normalized * M_PI / 2);
-	return ((int)(sin_value * 255));
-}
 
 void	fill_color_map(t_fractal_data *data, int (*color_func)(int), int bg)
 {

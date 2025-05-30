3ed57f5f951752705621f7bdd821ddb7009550ab
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-05-30 14:47:59 +0300

    fix(animation): Cleaned up animation function, unneeded code removed

--- src/animation.c
+++ src/animation.c
@@ -6,7 +6,7 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 18:08:28 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/05/27 18:50:59 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/05/30 13:26:41 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,8 @@ int	animate_julia(void *generic_data)
 		return (0);
 	data->animation_index = (data->animation_index + 1) % MAX_ANIMATION_STEPS;
 	angle = ((double)data->animation_index / MAX_ANIMATION_STEPS) * 2.0 * M_PI;
-	data->julia_param.real = 0.7885 * cos(angle);
-	data->julia_param.imag = 0.7885 * sin(angle);
-	data->animation_timer += 0.01;
-	if (data->animation_timer > 0.02)
-	{
-		data->animation_index = (data->animation_index + 1) % (data->win_width
-				* data->win_height);
-		data->animation_timer = 0.0;
-	}
+	data->julia_param.real = JULIA_RADIUS * cos(angle);
+	data->julia_param.imag = JULIA_RADIUS * sin(angle);
 	update_and_display(data);
 	return (0);
 }
